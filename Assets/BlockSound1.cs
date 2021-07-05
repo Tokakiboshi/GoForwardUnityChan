dd959d2fdeb4a817737d123384a5a594f6569ba5
Tokakiboshi <84213099+Tokakiboshi@users.noreply.github.com>
2021-07-06 01:19:11 +0900

    課題（音を出す）

---
+++ Assets/BlockSound1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSound1 : MonoBehaviour
+{
+
+    public AudioClip block;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Collistion");
+        audioSource.PlayOneShot(block);
+       
+    }
+
+}
